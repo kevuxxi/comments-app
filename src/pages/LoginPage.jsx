cd62bbf17f22aa7cffb797d44989ea5e5dbf00dd
kevin <kevinmpz10@icloud.com>
2025-10-27 12:57:51 -0300

    🧩 feat(login-page): connect login flow with redux and saga

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import { loginRequest } from '../features/auth/authSlice';
 
 const LoginPage = () => {
+    const dispatch = useDispatch();
+    const { loading, error, token } = useSelector((state) => state.auth)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    const handleLogin = () => {
+        e.preventDefault();
+        dispatch(loginRequest({ username, password }))
+    }
+
+    if (token) {
+        navigate("/comments");
+    }
+
     return (
-        <div>LoginPage</div>
+        <div>
+            <h2>Iniciar sesión</h2>
+
+            {error && <p>{error}</p>}
+            {loading && <p>Cargando...</p>}
+
+            <form onSubmit={handleLogin} >
+                <input
+                    type="text"
+                    placeholder='Usuario'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+
+                <input
+                    type='password'
+                    placeholder='Contraseña'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <button disabled={loading}>
+                    {loading ? "Ingresando..." : "Login"}
+                </button>
+            </form>
+
+
+        </div>
     )
 }
 

816e26309a3daeafcc3be4dfd8cc82749f1901a4
kevin <kevinmpz10@icloud.com>
2025-11-03 17:18:34 -0300

    📝 feat(comment-form): add create comment form with redux-saga integration

--- src/pages/CommentsPage.jsx
+++ src/pages/CommentsPage.jsx
@@ -1,17 +1,27 @@
 import { useDispatch, useSelector } from "react-redux"
-import { useEffect } from "react";
-import { fetchCommentsRequest } from "../features/comments/commentsSlice";
+import { useEffect, useState } from "react";
+import { createCommentRequest, fetchCommentsRequest } from "../features/comments/commentsSlice";
 import CommentCard from "../components/CommentCard";
 
 const CommentsPage = () => {
 
     const dispatch = useDispatch();
     const { comments, error, loading } = useSelector((state) => state.comments)
+    const [textareaValue, setTextareaValue] = useState('');
 
 
     useEffect(() => {
         dispatch(fetchCommentsRequest())
     }, [])
+
+    handleChange = (event) => {
+        setTextareaValue(event.target.value);
+    }
+
+
+    const handleSudmit = () => {
+        dispatch(createCommentRequest({ textareaValue }))
+    }
 
 
     return (
@@ -19,6 +29,21 @@ const CommentsPage = () => {
             <h2>Comentarios</h2>
             {error && <p>{error}</p>}
             {loading && <p>Cargando...</p>}
+
+            <form onSubmit={handleSudmit}>
+                <label>
+                    Comentar:
+                    <textarea
+                        value={textareaValue}
+                        onChange={handleChange}
+                        rows={5}
+                        cols={30}
+                    />
+                </label>
+                <p>Comentario actual: {textareaValue}</p>
+
+                <button type="submit">Enviar</button>
+            </form>
 
             {comments.map((comment) => (<li key={comment.id}><CommentCard comment={comment} /></li>))}
         </div>

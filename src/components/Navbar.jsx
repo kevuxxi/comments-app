f2b1e2f7cbbbe2fb1fe56e1f03dfcd8f66b497fd
kevin <kevinmpz10@icloud.com>
2025-11-04 15:03:50 -0300

    🔐 feat(navbar): display logged user and add logout functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../features/auth/authSlice';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+    const user = useSelector((state) => state.auth.user);
+    const token = useSelector((state) => state.auth.token);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+
+    const handleLogout = () => {
+        dispatch(logout())
+        navigate("/login");
+    }
+    useEffect(() => {
+        if (!token) {
+            navigate("/login");
+        }
+    }, [token, navigate]);
+
     return (
-        <div>Navbar</div>
+        <div>
+            <h1>Hola, {user?.username || 'Invitado'}</h1>
+            <button onClick={handleLogout}>Logout</button>
+        </div>
     )
 }
 
